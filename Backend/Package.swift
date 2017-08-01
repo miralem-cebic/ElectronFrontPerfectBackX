9034ddabc27a2ad2c4cd6c16ed1b1b4d92b75372
Miralem Cebic <miralem.cebic@matrix42.com>
2017-08-01 12:02:32 +0200

    simple test

--- Backend/Package.swift
+++ Backend/Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 let package = Package(
     name: "App",
     dependencies: [
-        .Package(
-        url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
-        majorVersion: 2
-        )
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
     ]
 )
--- Backend/Sources/main.swift
+++ Backend/Sources/main.swift
@@ -1,28 +1,49 @@
 import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
+import PerfectLogger
 
-// Create HTTP server.
+// --------
+LogFile.location = "./customLogFile.log"
+let eventid = LogFile.info("Server Setup message")
 let server = HTTPServer()
 
-// Register your own routes and handlers
-var routes = Routes()
-routes.add(method: .get, uri: "/", handler: {
-    request, response in
-    response.setHeader(.contentType, value: "text/html")
-    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-    response.completed()
-}
-)
+// --------
+// ROUTES
 
-// Add the routes to the server.
+var routes = Routes(baseUri:"/v1")
+
+func JSONMessage(message: String, response: HTTPResponse) {
+    do {
+        try response.setBody(json: ["message" : message])
+            .setHeader(.contentType, value: "application/json")
+            .completed()
+    } catch {
+        response.setBody(string: "Error in handling request: \(error)")
+            .completed(status: .internalServerError)
+    }
+}
+
+
+routes.add(method: .get, uri: "/hello", handler: {
+    request, response in
+    JSONMessage(message: "Hello, JSON!", response: response)
+})
+
+// --------
+// SERVER
+
 server.addRoutes(routes)
 
-// Set a listen port of 8181
+
 server.serverPort = 8181
+server.documentRoot = "webroot"
+
+LogFile.info("Server port is: \(server.serverPort)", eventid: eventid)
+LogFile.debug("Demonstrating a logging event without the linked event id")
 
 do {
-    // Launch the HTTP server.
+    LogFile.info("Starting server", eventid: eventid)
     try server.start()
 } catch PerfectError.networkError(let err, let msg) {
     print("Network error thrown: \(err) \(msg)")
